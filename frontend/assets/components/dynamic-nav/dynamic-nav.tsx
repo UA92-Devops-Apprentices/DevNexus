15ab1e41ada8bbce4616a0f10f357cedad88487c
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-06-06 09:22:56 +0100

    made a facsimile of what the end UI might look like
    just testing for the most part

--- frontend/assets/components/dynamic-nav/dynamic-nav.tsx
+++ frontend/assets/components/dynamic-nav/dynamic-nav.tsx
@@ -1,12 +1,29 @@
+'use client';
 
+import { useState } from "react";
 
 
 
 export default function DynamicNav() {
+
+    const [status, setStatus] = useState("open");
+
+    function toggleShow() {
+        switch (status) {
+            case "open":
+                setStatus("closed");
+                return;
+            case "closed":
+                setStatus("open");
+                return;
+        }
+    }
+
     return (
         // This nav element will frame the box
-        <nav className="bg-red"> 
-            <h1 className="">hi</h1>
+        <nav className="bg-red-500">
+            <h2 data-state={status} className="data-[state=open]:animate-open data-[state=open]:-ml-36 data-[state=closed]:animate-close data-[state=closed]:ml-0 fixed left-64 top-20 bg-blue-600 w-48 h-96 rounded-3xl">hidden element</h2>
+            <h1 onClick={()=>{toggleShow()}} className="fixed left-64 top-20 bg-red-500 w-1/2 rounded-3xl h-96">hi</h1>
         </nav>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        open: {
+          '0%': {marginLeft: '0px'},
+          '100%': {marginLeft: '-144px'}
+        },
+        close: {
+          '0%': {marginLeft: '-144px'},
+          '100%': {marginLeft: '0px'}
+        }
+      },
+      animation: {
+        'open': 'open 300ms ease-in',
+        'close': 'close 300ms ease-in'
+      }
     },
   },
   plugins: [],
